ab71b29e880e4bc08487e03c48f874a56ea0ee96
juniormartinez1995 <engmartinez00@gmail.com>
2020-10-16 14:38:11 -0300

    Classe da célula do mapa

---
+++ A-star Search Algorithm/A-star Search Algorithm/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A_star_Search_Algorithm
+{
+    class Tile
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Cost { get; set; }
+        public int Distance { get; set; }
+        public int CostDistance => Cost + Distance;
+        public Tile Parent { get; set; }
+
+        public void SetDistance(int targetX, int targetY)
+        {
+            this.Distance = Math.Abs(targetX - X) + Math.Abs(targetY - Y);
+        }
+    }
+}
